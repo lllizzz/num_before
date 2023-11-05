6bacdf7c2a9beea374a8cf16816dac229f44d7fe
Elizaveta <elizaveta.shetkina@gmail.com>
2023-11-05 23:38:47 +0300

    num_before

---
+++ num_before.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+    unordered_set<int> numbers;
+    int num;
+    while (cin >> num)
+    {
+        if (numbers.contains(num))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        numbers.insert(num);
+    }
+}
